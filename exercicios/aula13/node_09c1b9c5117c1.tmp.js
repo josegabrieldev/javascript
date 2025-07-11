a93f6e23fd63bb08ff5bb54085838b823243d7a3
José Gabriel <devgabrielsilva21@gmail.com>
2025-07-11 16:37:43 -0300

    aula 13 repetições

---
+++ exercicios/aula13/node_09c1b9c5117c1.tmp.js
@@ -0,0 +1,20 @@
+var data = new Date()
+var c = data.getSeconds()
+while (c > 10) {
+    console.log(`Olá, Mundo! ${c}`)
+    c++ // c = c + 1
+}
+
+
+
+
+/*
+ METODO NÃO RECOMENDADO
+console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+*/
---
+++ exercicios/aula13/node_66939cef539e2.tmp.js
@@ -0,0 +1,27 @@
+var c = 1
+do {
+    console.log(`Seja Bem vindo pela ${c} vez.`)
+} while (c <= 6)
+
+
+
+/*
+METDO RECOMENDADO
+while (c <= 6) {
+    console.log(`Olá, Mundo! ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+
+
+/*
+ METODO NÃO RECOMENDADO
+console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+*/
