2fd68558eefe64dd8134716e44bb533cf938fa69
José Gabriel <devgabrielsilva21@gmail.com>
2025-07-12 19:32:17 -0300

    aula14 finalizada

---
+++ exercicios/aula14/ambiente.js
@@ -0,0 +1,11 @@
+/*
+var c = 1
+while ( c <= 5) {
+    console.log(`O valor de C agora é ${c}`)
+    c++
+}
+*/
+//-------------------------------------------------------------
+for (var c = 1; c <=5; c++) {
+    console.log(`O Valor de C agora é ${c}!`)
+}
