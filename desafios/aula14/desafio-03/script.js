882ca343ae73b7d4f122c89ef1e0caa2b4a85260
José Gabriel <devgabrielsilva21@gmail.com>
2025-07-14 14:46:08 -0300

    desafio 04 concluido

--- desafios/aula14/desafio-03/script.js
+++ desafios/aula14/desafio-03/script.js
@@ -6,14 +6,28 @@ function contar() {
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO] Faltam Dados! Verifique e TENTE NOVAMENTE!')
+        res.innerHTML = 'Impossivel Contar!'
     } else {
-       res.innerHTML = 'Contando.. '
+       res.innerHTML = 'Contando... <br>'
        let i = Number(inicio.value)
        let f = Number(fim.value)
        let p = Number(passo.value)
 
-       for () {
-        
+       if (p <= 0) {
+            window.alert('Passo Inválido! Considerando passo 1')
+            p = 1
        }
+       if (i < f) {
+        // Contagem Crescente
+            for (var c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449} `
+            }
+       } else {
+        // Contagem Regressiva
+            for (var c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+       }
+       res.innerHTML += `\u{1F3F4}`
     }
 }
