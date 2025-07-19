b96f68e223d90cbe29f7f651bb58cf46c62a0c44
José Gabriel <devgabrielsilva21@gmail.com>
2025-07-19 02:10:24 -0300

    Inicio de variáveis compostas

---
+++ exercicios/aula15/ambiente.js
@@ -0,0 +1,6 @@
+let num = [44, 9, 57, 4, 5, 7, 1, 92, 11]
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} elementos.`)
+console.log(`${num}`)
---
+++ exercicios/aula15/vetornatela.js
@@ -0,0 +1,15 @@
+let num = [9, 1, 2, 7, 4, 6]
+//console.log(num)
+
+/*
+console.log(num[0])
+console.log(num[1])
+console.log(num[2])
+console.log(num[3])
+console.log(num[4])
+console.log(num[5])
+*/
+
+for (let pos = 0; pos < num.length; pos++) {
+    console.log(`O elemento ${pos} tem o valor ${num[pos]}`)
+}
