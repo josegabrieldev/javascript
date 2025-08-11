a67190c2930aba6f847a11e0f4987a2e49b1dd2b
José Gabriel <devgabrielsilva21@gmail.com>
2025-08-10 22:43:03 -0300

    ambiente aula13

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,30 @@
+// BOM METODO TAMBEM
+var c = 1
+do {
+    console.log(`Seja Bem vindo pela ${c} vez.`)
+    c++ // c = c + 1
+} while (c <= 5)
+
+
+
+/*
+METODO RECOMENDADO
+var c = 1
+while (c <= 6) {
+    console.log(`Olá, Mundo! ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+
+
+/*
+ METODO NÃO RECOMENDADO
+console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+*/
