7746f69a3d3c1e2433fb994d85e3e12f8297cdcf
José Gabriel <devgabrielsilva21@gmail.com>
2025-08-11 01:33:54 -0300

    script02 aula06

---
+++ atividades/aula06/script02.js
@@ -0,0 +1,40 @@
+/* Variaveis
+
+    const (Basicamente não pode ser atribuido outro valor)
+    let (Pode atribuir outro valor)
+    var (Não é recomendado usar)
+
+*/
+
+const nome = 'Gabriel'
+let idade = 21
+var casado = false
+
+/* Tipos de Dados
+
+    Strings -> Textos
+    Numbers -> Números
+    Boolean -> Booleano (True ou False)
+    Object -> Objetos
+    Null & Undefinid
+    Array -> Matriz
+*/
+
+// objeto
+const usuario1 = {
+    nome: 'Gabriel',
+    idade: 21,
+    casado: false
+}
+
+const usuario2 = {
+    nome: 'José',
+    idade: 19,
+    casado: true
+}
+
+console.log(usuario1)
+
+const meuArray = [21, 32, 43, 40, 25, 6.5]
+console.log(meuArray)
+console.log(usuario2.nome)
