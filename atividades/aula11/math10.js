5689bbc7faa41828052f064208552ad0d3727150
José Gabriel <devgabrielsilva21@gmail.com>
2025-08-14 19:12:40 -0300

    Math comandos aula11

--- atividades/aula11/math10.js
+++ atividades/aula11/math10.js
@@ -1,7 +1,11 @@
 /*
         Math --> Matemática
         - pow(2,2) / potência / 2 elevado a 2 (2x2) = 4
+        - sqrt(25,2) / raiz quadrada
+        - ceil / teto -> arredonda para cima
+        - floor / chão -> arredonda para baixo
+        - random() / número aleatório entre 0 e 1
 */
 
-const calculo = Math.pow(2,2)
-console.log(calculo)
+const calculo = Math.random() * (9999 - 0) + 1 // da um código de 4 digitos aleatoriamente
+console.log(Math.ceil(calculo))
