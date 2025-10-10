001694adb896633d58f1f32b5b347f42525da199
José Gabriel <devgabrielsilva21@gmail.com>
2025-10-10 00:08:18 -0300

    Operadores aritmeticos

--- tarefas/t1/script.js
+++ tarefas/t1/script.js
@@ -1,3 +1,5 @@
-const idade = prompt("Digite a sua idade")
-const nomeCompleto = prompt('Me diga seu nome completo')
-alert(`Você tem ${idade} anos e seu nome é ${nomeCompleto}`)
+let idade, pessoa, res;
+idade = 21
+pessoa = (idade < 18) ? `Você é menor de idade, possui ${idade}` : `Você é maior de idade, possui ${idade}`;
+res = (idade < 18 || idade > 65) ? "MENOR DE IDADE" : "MAIOR DE IDADE";
+document.getElementById("texto").innerHTML = res;
