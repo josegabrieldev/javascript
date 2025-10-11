03098f525816c4c4f909cbdcaf1f382807a77f00
José Gabriel <devgabrielsilva21@gmail.com>
2025-10-10 22:11:54 -0300

    t1 em tarefas de JS

--- tarefas/t1/script.js
+++ tarefas/t1/script.js
@@ -1,5 +1,9 @@
-let idade, pessoa, res;
-idade = 21
-pessoa = (idade < 18) ? `Você é menor de idade, possui ${idade}` : `Você é maior de idade, possui ${idade}`;
-res = (idade < 18 || idade > 65) ? "MENOR DE IDADE" : "MAIOR DE IDADE";
-document.getElementById("texto").innerHTML = res;
+const usu = { 
+    nome: document.getElementById("nome"),
+    idade: document.getElementById("idade"),
+    cidade: document.getElementById("cidade"),
+    profissao: document.getElementById("profissao")
+};
+function mensagem() {
+     document.getElementById("texto").innerHTML = `Olá, eu sou ${usu.nome.value}, tenho ${usu.idade.value} anos, moro em ${usu.cidade.value} e sou ${usu.profissao.value}.`;
+}
