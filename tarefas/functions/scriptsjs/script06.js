9dab5a065c569ad0fb58ef1fdbcd3c10415ddc25
José Gabriel <devgabrielsilva21@gmail.com>
2025-10-13 11:50:01 -0300

    functions ex06

--- tarefas/functions/scriptsjs/script06.js
+++ tarefas/functions/scriptsjs/script06.js
@@ -1,8 +1,10 @@
 function inverterString(event) {
     event.preventDefault(); // Evita o envio do formulário
+    const resultadoDiv = document.getElementById('resultado');
     const inputString = document.getElementById('inputString');
     let textoSeparado = inputString.value.split('');
     let textoReverse = textoSeparado.reverse();
     let palavra = textoReverse.join('');
-    console.log(palavra);
+    resultadoDiv.innerHTML = `Palavra Invertida: ${palavra}`;
+    inputString.value = '';
 }
