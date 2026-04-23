82ddc91eb8905ff7f34bdbcd4efa3f9d32658bf7
José Gabriel <devgabrielsilva21@gmail.com>
2026-04-23 01:17:16 -0300

    fundamentos
    ex01

---
+++ algoritmo/fundamentos-js/01-variaveis/ex01.js
@@ -0,0 +1,13 @@
+const nome = "José Gabriel";
+const idade = 22;
+const maiorDeIdade = true;
+const timeFavorito = "Palmeiras";
+const cnh = null;
+
+console.log(nome);
+console.log(idade);
+console.log(maiorDeIdade);
+console.log(timeFavorito);
+console.log(cnh);
+
+console.log(`Me chamo ${nome}, tenho ${idade} anos. Sou maior de idade? ${maiorDeIdade}. Meu time favorito é ${timeFavorito}. Tenho CNH? ${cnh}`)
