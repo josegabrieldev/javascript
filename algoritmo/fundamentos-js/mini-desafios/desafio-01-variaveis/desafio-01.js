f1a2dde154e694e8e829aac543600a1042ec7bd0
José Gabriel <devgabrielsilva21@gmail.com>
2026-04-27 02:44:13 -0300

    desafio-01 variaveis

---
+++ algoritmo/fundamentos-js/mini-desafios/desafio-01-variaveis/desafio-01.js
@@ -0,0 +1,38 @@
+let nomeProduto = "Teclado Gamer";
+let precoProduto = 102.58;
+let quantidadeProduto = 7;
+let produtoDisponivel = null;
+let categoriaPrincipalProduto = "Computador";
+
+if (quantidadeProduto > 0) {
+    produtoDisponivel = true;
+} else {
+    produtoDisponivel = false;
+};
+
+let produto = {
+    nome: nomeProduto,
+    preco: precoProduto,
+    estoque: quantidadeProduto,
+    disponivel: produtoDisponivel,
+    categoria: categoriaPrincipalProduto,
+    desconto: null
+};
+
+produto.desconto = 40;
+
+let categoriaExtras = ["Tecnologia", "Acessório", "Entrada de comandos"];
+
+// Valida melhor o produto disponivel!
+let msgDisponibilidade = "";
+if (produto.disponivel === true) {
+    msgDisponibilidade = `Temos ${produto.estoque} ${produto.nome} disponiveis no momento.`;
+} else {
+    msgDisponibilidade = `No momento não temos ${produto.nome} disponiveis, Agradecemos a preferência.`;
+};
+
+// Aplicando desconto ao produto
+let produtoComDesconto = produto.preco - produto.desconto;
+
+// Mostrar todos os dados do objeto
+console.log(`Dados do produto; Nome: ${produto.nome}, Preço: R$${produto.preco}, Estoque: ${produto.estoque}, Disponivel? ${msgDisponibilidade} Categoria Principal: ${produto.categoria}, Desconto de R$${produto.desconto}. Preço final com desconto, de R$${produto.preco} por apenas R$${produtoComDesconto}. Categorias Extras do produto são: ${categoriaExtras[0]}, ${categoriaExtras[1]}, ${categoriaExtras[2]}.`)
