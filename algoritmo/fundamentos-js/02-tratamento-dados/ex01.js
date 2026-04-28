00f07665a78048a84d888ce216560db14855ee84
José Gabriel <devgabrielsilva21@gmail.com>
2026-04-28 00:56:08 -0300

    ex01 tratamento de dados

---
+++ algoritmo/fundamentos-js/02-tratamento-dados/ex01.js
@@ -0,0 +1,8 @@
+const numero1 = "44";
+const numero2 = "23";
+
+const n1 = Number(numero1);
+const n2 = Number(numero2);
+
+const soma = n1 + n2;
+console.log(`A soma entre ${numero1} e ${numero2} é igual a ${soma}`);
