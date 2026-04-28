c0b91667f56052dcdb524c3de0619a92ac947678
José Gabriel <devgabrielsilva21@gmail.com>
2026-04-28 01:55:31 -0300

    ex03 tratamento de dados

---
+++ algoritmo/fundamentos-js/02-tratamento-dados/ex03.js
@@ -0,0 +1,9 @@
+const textoNumero = "150.5";
+const valorNulo = null;
+const booleanoTexto = true;
+
+console.log(`
+    String => Number: ${Number(textoNumero)}
+    Valor nulo: ${valorNulo}
+    Boolean => String: ${booleanoTexto.toString()}
+    `);
