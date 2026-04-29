cb587fe8427d6f6feb02b65a403f1e598fa20a71
José Gabriel <devgabrielsilva21@gmail.com>
2026-04-29 01:06:48 -0300

    ex02 operadores

---
+++ algoritmo/fundamentos-js/03-operadores/ex02.js
@@ -0,0 +1,8 @@
+const idade = 19;
+const pontuacao = 65;
+
+if (idade >= 18 && pontuacao >= 70) {
+    console.log("Você atende aos requisitos e passou no teste. Parabéns, seja bem vindo a empresa.")
+} else {
+    console.log("Infelizmente você não atende aos requisitos necessarios e falhou no teste. Continue tentando.")
+};
